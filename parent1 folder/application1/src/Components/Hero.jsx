a4af35e422c0827929ece3a4e4a443a6d2257aec
Erastus <erastusngamau8@gmail.com>
2024-06-16 15:13:28 +0300

    first commit

---
+++ parent1 folder/application1/src/Components/Hero.jsx
@@ -0,0 +1,17 @@
+const Hero = () => {
+    return (
+    <div className='text-white'>
+    
+    <div className='max-w-[800px] at-[-96px] w-full h-screen mx-auto text-center flex-col justify-center'>
+    <p className='text-[#00df9a] font-bold p-2'>GROWING WITH REACT</p>
+    <h className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>Grow with react.</h>
+    <p className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>Fast,Flexible,Reliable </p>
+    <Typed strings={['BTB','BTC','SASS']} 
+    typeSpeed={120} backSpeed={140} 
+    loop
+    />
+        </div>
+    </div>
+    )
+    }
+    export default Hero
