944f80020b101327c837615238abd987cc6d801e
Erastus <erastusngamau8@gmail.com>
2024-06-16 15:09:22 +0300

    first commit

---
+++ parent1 folder/web1/src/App.js
@@ -0,0 +1,19 @@
+
+import React from 'react'
+import Navbar from  './Components/Navbar';
+import Hero from './Components/Hero';
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+    </div>
+  )
+   
+        
+    
+ 
+}
+
+export default App;
