61aac461a2c34978e8f2070c411196687a4f9993
steve <steve@rapidream.com>
2024-06-25 13:48:52 +0300

    Fixes

--- jobbs/src/App.js
+++ jobbs/src/App.js
@@ -4,28 +4,20 @@ import Navbar from "./Components/Navbar";
 import Home from "./Pages/Home";
 import Trending from "./Pages/Trending";
 import About from "./Pages/About";
-import Dream from "./Components/Dream";
 
 const App = () => {
   return (
-  
-      <Router>
-      
-        <Navbar />
-        <Dream/>
-  
-        <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/home" element={<Home/>} />
-          <Route path="/trending" element={<Trending />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      
-      </Router>
-    
-  );
+    <Router>
+      <Navbar />
 
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/trending" element={<Trending />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
-
