6668418ef724000f91b53073219c2edec1574564
erastus ngamau <erastusngamau8@gmail.com>
2024-07-07 23:58:12 +0300

    updated version

--- figmacode1/src/Pages/Marketplace.jsx
+++ figmacode1/src/Pages/Marketplace.jsx
@@ -4,7 +4,7 @@ import logo512 from "../assets/logo512.png";
 const Marketplace = () => {
   return (
     <div className="bg-blue-950 min-h-screen flex justify-start items-start pt-12 px-4">
-      <div className="bg-blue-950 rounded-lg w-full max-w-[523px] md:w-auto md:max-w-[523px] ml-8">
+      <div className="bg-blue-950 rounded-lg w-full  max-w-[523px] md:w-auto md:max-w-[523px] ml-8">
         <div className="text-start px-12 text-blue-600">
           <h2 className="text-3xl font-bold mb-2">GET YOUR OG EDITION</h2>
           <h2 className="text-3xl font-bold mb-2">MYTHICAL BUDDY!</h2>
@@ -15,47 +15,46 @@ const Marketplace = () => {
             FREE mint of 1988 OG Edition Mythical Buddy NFTs
           </p>
         </div>
-
-        <div className="w-full md:w-[500px] md:flex md:flex-col max-w-md mx-auto md:absolute md:right-0 md:m-8 md:top-32 md:flex-end h-[750px]">
+        <div className="w-full md:w-[500px] md:flex md:flex-col max-w-md mx-auto md:absolute md:right-0 md:m-2 md:top-32 md:flex-end h-[550px] mb-0">
           <img src={logo512} alt="logo512" className="mx-auto md:mx-0" />
-          <div className="bg-blue-950 absolute bottom-0 w-full md:w-[430px] h-[52px]">
-            <span className="text-white text-xl gap-60 text-bold flex-1 px-12">
+          <div className="bg--950 justify-items-center m-2 mt-[-98px] w-[430px] h-[52px]">
+            <span className="text-white justify-center text-lg text-bold my-12 px-20">
               Round1Quota
-              <button className="bg-blue-800 justify-self-end mx-4 gap-x-10 w-24 h-7">
+              <button className="bg-blue-800 my-4 mx-6 w-24 h-7">
                 <span className="text-white text-xl text-bold">1588</span>
               </button>
             </span>
           </div>
         </div>
-        <div className="text-white md:flex md:flex-col md:grid-cols-2 py-10 px-12 mt-2">
-          <button className="w-full md:w-[635px] h-[70px] p-4 flex items-center bg-gradient-to-r from-[#59256D] to-[#716896] rounded-tl-lg opacity-100">
+        <div className="text-white md:flex  md:flex-col max-w-md  md:grid-cols-2  mb-6 md-mb-2 py-1 px-12 md:my-120 mt-2">
+          <button className="w-full md:h-full display: inline-block md:w-[600px] h-[88px]  p-4  items-center bg-gradient-to-r from-[#59256D] to-[#716896] rounded-tl-lg opacity-100">
             <span className="ml-5 gap-2 text-2xl font-bold">COMING SOON</span>
           </button>
         </div>
         <div className="text-white px-12 mt-2">
           <div className="flex justify-start flex-row">
-            <h3 className="text-sm whitespace-nowrap py-1 mr-6 font-bold">
+            <h3 className="text-xl w-[233px] h-[36px] whitespace-nowrap py-1 mr-8 font-bold">
               Round 1 starts in:
             </h3>
-            <div className="flex text-white text-xl space-x-4 self-auto">
-              <button className="w-[97px] h-[36px] flex items-center justify-center bg-gradient-to-r from-[#4E37B3] to-[#59256D] rounded-lg opacity-100">
+            <div className="flex text-white text-xl space-x-4  ">
+              <button className="w-[64px]  h-[36px] flex items-center justify-center bg-gradient-to-r from-[#4E37B3] to-[#59256D] rounded-lg">
                 <span>01</span>
               </button>
               <span className="text-gray-600 font-bold">:</span>
-              <button className="w-[97px] h-[36px] flex items-center justify-center bg-gradient-to-r from-[#4E37B3] to-[#59256D] rounded-lg opacity-100">
+              <button className="w-[64px]  h-[36px] flex items-center justify-center bg-gradient-to-r from-[#4E37B3] to-[#59256D] rounded-lg">
                 <span>21</span>
               </button>
               <span className="text-gray-600 font-bold">:</span>
-              <button className="w-[97px] h-[36px] flex items-center justify-center bg-gradient-to-r from-[#4E37B3] to-[#59256D] rounded-lg opacity-100">
+              <button className="w-[64px] h-[36px] flex items-center justify-center bg-gradient-to-r from-[#4E37B3] to-[#59256D] rounded-lg">
                 <span>15</span>
               </button>
               <span className="text-gray-600 font-bold">:</span>
-              <button className="w-[97px] h-[36px] flex items-center justify-center bg-gradient-to-r from-[#4E37B3] to-[#59256D] rounded-lg opacity-100">
+              <button className="w-[64px] h-[36px] flex items-center justify-center bg-gradient-to-r from-[#4E37B3] to-[#59256D] rounded-lg">
                 <span>45</span>
               </button>
             </div>
           </div>
-          <div className="mt-8 flex-1 px-12 w-full hover:bg-sky-700 h-12 rounded-full bg-blue-400">
+          <div className="mt-8 flex-1 px-12 py-4 w-[265px] hover:bg-sky-700 h-[70px] rounded-full bg-blue-400">
             <div className="flex justify-center">
               <button className="mt-2">
                 <span>Sign in to mint</span>
